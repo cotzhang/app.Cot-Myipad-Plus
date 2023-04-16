fa92cd0671212e6dd01dab7e9e97d5891c95e5ee
cotzhang <3063925756@qq.com>
2023-04-16 09:32:09 +0800

    Update autoupdate system

--- applyupd.js
+++ applyupd.js
@@ -2,7 +2,7 @@ function APPLYUPD(upditems,alertid) {
 	var zip = new AdmZip(getuserdatapath() + '/app.zip')
 	zip.getEntries()
 	zip.extractAllTo(process.cwd(), true)
-	panelistic.dialog.confirm("更新", "软件更新完成，需要重启软件以应用更新" + upditems, "应用更新", "取消", (cf) => {
+	panelistic.dialog.confirm("更新", "软件更新完成，需要重启软件以应用更新<br><br>" + upditems, "应用更新", "取消", (cf) => {
 		if (cf) {
 			remote.app.relaunch()
 			remote.app.exit()
